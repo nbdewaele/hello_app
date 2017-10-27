fc7925aa2253add4ed88783b4c19b245d64e1959
Nate DeWaele <nbdewaele@gmail.com>
2017-10-27 10:23:50 -0400

    finished exercise and added emoji

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render html: "That aint my baby!"
+		render html: "That aint my baby! 😐"
 	end
 
 	def goodbye
